6c10ee3a5517a4935874ad4f253cf5131a733ddd
Hong Liang Zhao <66213415+jiemazhb@users.noreply.github.com>
2024-05-26 16:53:39 -0700

    add more
    nothing

--- BlogSystem/BlogSystem.MVCSite/Controllers/HomeController.cs
+++ BlogSystem/BlogSystem.MVCSite/Controllers/HomeController.cs
@@ -28,7 +28,7 @@ namespace BlogSystem.MVCSite.Controllers
         }
         public ActionResult Index()
         {
-            // for testing 
+            // for testing add more!!!!!!!!!!!!!!!!!!!!!!
             return View();
 
         }
